78f01b89f94d9127372774b52c074d69b203d9b1
fporcari <francesco.porcari@softwell.it>
2022-10-12 09:06:36 +0200

    profile page

---
+++ packages/comm/webpages/profile.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class GnrCustomWebPage(object):
+    py_requires='th/th:TableHandler'
+
+    def main(self, root, **kwargs):
+        root.thFormHandler(datapath='main',
+            table='comm.developer',
+            startKey=self.rootenv['developer_id'],
+            formResource='FormProfile'
+        )
