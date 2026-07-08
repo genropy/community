53d41a01a5c0fe35854bc5b9741c8b6d9b12afa6
Davide Paci <davide.paci@softwell.it>
2026-07-08 10:54:15 +0200

    refactor: keep only core menu entries in comm ApplicationMenu
    Optional-package menu entries (sbs, github, srvy, wpn, social, video,
    wordpress, genrobot, dem) moved to the gnrcomm base package in the
    softwellsrl/gnrcomm deployment project, which is the mainpackage of
    the full gnr_comm instance. This removes the package-presence guards:
    the comm menu is now plain and works with core packages only.

--- packages/comm/menu.py
+++ packages/comm/menu.py
@@ -36,44 +36,14 @@ class ApplicationMenu(object):
         root.webpage('!!Messages', filepath='/comm/my_messages', table='email.message', 
                          menuLineBadge='getMyUnreadMessages',
                          menucode='messages')
-        if 'sbs' in self.db.packages:
-            root.webpage('!![en]Subscriptions', filepath='/sbs/user_subscriptions')
         root.webpage(u"!![en]Community map", filepath="/comm/community_map")
-        if 'social' in self.db.packages or 'wordpress' in self.db.packages:
-            self.publicationsSubMenu(root.branch("!![en]Publications"))
         #root.thpage(u"!![en]Suggestions", table="comm.suggestion")
         #root.thpage(u"!![en]Projects", table="comm.project", viewResource='ViewDevelopers', formResource='FormDevelopers')
         #root.thpage(u"!![en]Events", table="comm.event", viewResource='ViewDevelopers', formResource='FormDevelopers')
         
-    def publicationsSubMenu(self, branch):
-        if 'social' in self.db.packages:
-            branch.webpage('!![en]Social posts', filepath='/comm/app_dev/social_posts', menucode='social_posts')
-        if 'wordpress' in self.db.packages:
-            branch.webpage('!![en]Blog posts', filepath='/comm/app_dev/blog_posts', menucode='blog_posts')
-
     def config(self,root,**kwargs):
-        packages = self.db.packages
         root.packageBranch(u"!![en]Community", pkg="comm")
-        if 'sbs' in packages:
-            root.packageBranch(u"!![en]Subscriptions management", pkg="sbs")
-        if 'github' in packages:
-            root.packageBranch(u"!![en]GitHub", pkg="github")
-        if 'srvy' in packages:
-            root.packageBranch(u"!![en]Surveys", pkg="srvy", tags='admin')
-        if self.db.application.getPreference('enable_social', pkg='comm'):
-            if 'social' in packages:
-                root.packageBranch(u"!![en]Social", pkg="social", tags='admin')
-            if 'video' in packages:
-                root.packageBranch(u"!![en]Video", pkg="video", tags='admin')
-            if 'wordpress' in packages:
-                root.packageBranch('!![en]Blog', pkg='wordpress', tags='admin')
-        if 'genrobot' in packages and self.db.application.getPreference('enable_genrobot', pkg='comm'):
-            root.packageBranch(u"!![en]Genrobot", pkg="genrobot", tags='admin')
-        if 'dem' in packages and self.db.application.getPreference('enable_dem', pkg='comm'):
-            root.packageBranch(u"!![en]DEM", pkg="dem", tags='admin')
         root.packageBranch(u"!![en]E-mail", pkg="email", tags='admin')
         root.packageBranch(u"!![en]Administration", tags="superadmin,_DEV_", pkg="adm")
         root.packageBranch(u"!![en]System", tags="_DEV_", pkg="sys")
-        if 'wpn' in packages:
-            root.packageBranch("!![en]Web Notifications", pkg="wpn")
         root.packageBranch("!![en]Dashboard", pkg="biz")
